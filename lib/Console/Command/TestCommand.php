8c61b148d967597a2618e0ec340d23f8766e0587
Nicolas Brousse <pro@nicolas-brousse.fr>
2015-01-13 23:52:19 +0100

    Install test command draft

---
+++ lib/Console/Command/TestCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class TestCommand extends Command
+{
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('installer:test')
+            ->setDescription('Test the Subbly Installer')
+        ;
+    }
+
+    /**
+     *
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Compile
+
+        // Mv install
+
+        // Execute install
+
+        // Check the install
+
+        // Print install file info (like size)
+    }
+
+    /**
+     *
+     */
+    protected function compile()
+    {
+        $command = $this->getApplication()->find('installer:compile');
+        $input   = new ArrayInput(array());
+
+        if ($command->run($this->input, $this->output) === 0) {
+            // It works!
+        }
+    }
+}
