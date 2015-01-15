f7217e1fc390f177f50b32cf88e508abc562dc3b
Nicolas Brousse <pro@nicolas-brousse.fr>
2015-01-15 01:07:37 +0100

    Update I18n class

--- src/Subbly/Installer/I18n.php
+++ src/Subbly/Installer/I18n.php
@@ -2,16 +2,38 @@
 
 class Subbly_Installer_I18n
 {
+    const DEFAULT_LOCALE = 'en';
+
     /**
      * List of locales
      * @var array $locales
      */
     private static $locales = array(
         'en' => array(
-            'form.user.title' => 'User',
+            // requirements.php
+            'requirements.title' => 'Requirements',
+            'requirements.text'  => 'Some php modules are missing :/.',
+
+            // install.php
+            'form.generic.title'     => 'Common settings',
+            'form.generic.shop_name' => 'Shop name',
+
+            'form.user.title'    => 'User settings',
+            'form.user.email'    => 'Email',
+            'form.user.password' => 'Password',
+
+            'form.db.title' => 'Database settings',
+            'form.db.host' => 'Database host',
+            'form.db.name' => 'Database name',
+            'form.db.username' => 'Database username',
+            'form.db.password' => 'Database use password',
+            'form.db.prefix' => 'Tables prefix',
+
+            'form.submit' => 'Install!',
         ),
         'fr' => array(
-            'form.user.title' => 'Utilisateur',
+            // TODO
+            // 'form.user.title' => 'Utilisateur',
         ),
     );
 
@@ -23,10 +45,19 @@ class Subbly_Installer_I18n
      *
      * @return string
      */
-    public static function l($key, array $params = array())
+    public static function l($key, array $params = array(), $forceLocale = null)
     {
-        if (array_key_exists($key, self::$locales)) {
-            $message = self::$locales[$key];
+        $locale = $forceLocale;
+        if ($forceLocale === null) {
+            $locale = self::getLocale();
+        }
+
+        if (
+            array_key_exists($locale, self::$locales)
+            && array_key_exists($key, self::$locales[$locale])
+        ) {
+            $locales = self::$locales[$locale];
+            $message = $locales[$key];
 
             foreach ($params as $k => $v) {
                 $params[$k] = '{'.$v.'}';
@@ -35,6 +66,18 @@ class Subbly_Installer_I18n
             return str_replace(array_keys($params), array_values($params), $message);
         }
 
+        if ($locale !== self::DEFAULT_LOCALE) {
+            return self::l($key, $params, self::DEFAULT_LOCALE);
+        }
+
         return $key;
+    }
+
+    /**
+     *
+     */
+    public static function getLocale()
+    {
+        return 'fr'; // TODO dynamise it
     }
 }
